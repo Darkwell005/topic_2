a714d133fd1b033cbb677b7a0b2f17e3577f79a7
Alvin <darkwell463@gmail.com>
2023-06-22 21:20:32 +0300

    Решение V2 Домашнее задание | 2. Консольные операции
    - Решение задач: 0,1,3,4,5,6,7,8,9.

--- tasks/task_7.py
+++ tasks/task_7.py
@@ -4,4 +4,4 @@ quantity = 3
 #Необходимо вычесть из 100%-20%,это будет составлять % единицы предмета со скидкой
 discount1 = (100 - discount) * cost / 100
 total_cost = discount1 * quantity
-print('Стоимость вашего заказа:',total_cost)
+print('Стоимость вашего заказа:',total_cost,'рублей')
